4d01d39a7fb599398e88d5ccb78d32533d0dc8ba
morris2005 <morris.treutner@student.hnu.de>
2026-01-09 18:37:42 +0100

    ActionListener für den Speicher Button hinzugefügt und fertig programmiert, sodass die eingegeben Daten im Fenster über den Speicher Button, als ein neues Objekt in die ArrayList gespeichert werden. Zudem noch einen ActionListener für die buchartComboBox, sodass jenachdem was ausgewählt wurde, in dem leihfristTextField ein bestimmter Text erscheint.

--- src/main/java/schulbibliothek/Schulbibliothek.java
+++ src/main/java/schulbibliothek/Schulbibliothek.java
@@ -1,6 +1,9 @@
 package schulbibliothek;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Schulbibliothek extends JFrame {
     private JPanel hauptPanel;
@@ -26,6 +29,67 @@ public class Schulbibliothek extends JFrame {
     private JButton filterButton;
     private JLabel leihfristLabel;
     private JTextField leihfristTextField;
+    private ArrayList<Buch> buch = new ArrayList<>();
+
+
+    public Schulbibliothek() {
+        speicherButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                speichern();
+            }
+        });
+        buchartComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Combobox ActionListener um im Leihfrist Textfeld den Wert anzuzeigen
+                String ausgewaehlt = buchartComboBox.getSelectedItem().toString();
+                    if (ausgewaehlt.equals("Taschenbuch")) {
+                        leihfristTextField.setText("4 Wochen");
+                    } else if (ausgewaehlt.equals("Hardcover")) {
+                        leihfristTextField.setText("3 Wochen");
+                    } else if (ausgewaehlt.equals("Broschur")) {
+                        leihfristTextField.setText("2 Wochen");
+                    } else
+                        leihfristTextField.setText("unbegrenzt");
+
+            }
+        });
+    }
+    public void speichern() {
+            //Werte aus den TextFields,der ComboBox und der CheckBox holen und in diesen Variablen speichern
+            String name = nameTextField.getText();
+            String autor = autorTextField.getText();
+            String fach = fachTextField.getText();
+            String verlag = verlagTextField.getText();
+            int isbn = Integer.parseInt(isbnTextField.getText());
+            String buchart = buchartComboBox.getSelectedItem().toString();
+            boolean vorhanden = vorhandenCheckBox.isSelected();
+            String leihfrist;
+            if (buchart.equals("Taschenbuch")) {
+                leihfrist = "4 Wochen";
+            } else if (buchart.equals("Hardcover")) {
+                leihfrist = "3 Wochen";
+            } else if (buchart.equals("Broschur")) {
+                leihfrist = "2 Wochen";
+            } else
+                leihfrist = "unbegrenzt";
+
+            //Objekt erstellen und in der ArrayList speichern
+            Buch b = new Buch(name,autor,fach,verlag,buchart,isbn,vorhanden,leihfrist);
+            buch.add(b);
+
+            //Nach dem speichern die Textfelder leeren um erneut reinschreiben zu können
+            nameTextField.setText("");
+            autorTextField.setText("");
+            fachTextField.setText("");
+            verlagTextField.setText("");
+            isbnTextField.setText("");
+            buchartComboBox.setSelectedIndex(0);
+            vorhandenCheckBox.setSelected(false);
+            leihfristTextField.setText("");
+
+    }
 
     //Main zum Fenster ausführen über FormMain generiert
     public static void main(String[] args) {
